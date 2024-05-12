ce1b5483a5eb90a9d9039b2576a7b92e5bb97ec0
xioperez01 <angie.xioperez@gmail.com>
2024-05-12 01:12:23 -0500

    🔧 update packages and config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#F85900",
+        softOrange: "#F8C100",
+        lightOrange: "#FFB74B",
+        secondary: "#B9B9B9",
+        softGrey: "#CACACA",
+        lightGrey: "#EEEEEE",
+      },
+      backgroundImage: {
+        gradient_sideBar: "linear-gradient(#F85900 5%, #FECB47)",
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
   },
   plugins: [],
 };
